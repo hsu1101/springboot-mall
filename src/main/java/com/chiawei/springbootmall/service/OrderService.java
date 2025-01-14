06c019cc6dd0d26eb8994bb0ec12fcf920bcb0f5
hsu1101 <johnee6550@gmail.com>
2025-01-14 18:35:06 +0800

    訂單功能 - 創建訂單(下)

--- src/main/java/com/chiawei/springbootmall/service/OrderService.java
+++ src/main/java/com/chiawei/springbootmall/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.chiawei.springbootmall.service;
 
 import com.chiawei.springbootmall.dto.CreateOrderRequest;
+import com.chiawei.springbootmall.model.Order;
 
 public interface OrderService {
 
+    Order getOrderById(Integer orderId);
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
+
 }
--- src/main/java/com/chiawei/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/chiawei/springbootmall/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.chiawei.springbootmall.dao.OrderDao;
 import com.chiawei.springbootmall.dao.ProductDao;
 import com.chiawei.springbootmall.dto.BuyItem;
 import com.chiawei.springbootmall.dto.CreateOrderRequest;
+import com.chiawei.springbootmall.model.Order;
 import com.chiawei.springbootmall.model.OrderItem;
 import com.chiawei.springbootmall.model.Product;
 import com.chiawei.springbootmall.service.OrderService;
@@ -21,6 +22,15 @@ public class OrderServiceImpl implements OrderService {
     private OrderDao orderDao;
     @Autowired
     private ProductDao productDao;
+
+    @Override
+    public Order getOrderById(Integer orderId) {
+        Order order =  orderDao.getOrderById(orderId);
+
+        List<OrderItem> orderItemList = orderDao.getOrderItemsByOrderId(orderId);
+        order.setOrderItemList(orderItemList);
+        return order;
+    }
 
     @Transactional
     @Override
